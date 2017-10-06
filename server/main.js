99a8529f579a1f0040618428e4ef0e238352c21d
Stefano Capettini <ste.cape@gmail.com>
2017-10-06 19:36:32 +0200

    Google Auth

--- server/main.js
+++ server/main.js
@@ -8,9 +8,7 @@ ServiceConfiguration.configurations.upsert(
   { service: 'google' },
   {
     $set: {
-  		clientId: "1087941511441-vbg8naukooft9llagnvv80cg7t1gq527.apps.googleusercontent.com",
-  		secret: "BLzLiP13Waxg0rNZZpe80hvX",
-      	loginStyle: 'popup',
+    		blablabla
       },
     }
   );
