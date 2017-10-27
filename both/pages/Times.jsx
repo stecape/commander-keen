601ff7f032e973d3af4d8e555400a88371bbcadd
stecape <ste.cape@gmail.com>
2017-10-27 07:32:12 +0200

    chart css fixed

--- both/pages/Times.jsx
+++ both/pages/Times.jsx
@@ -9,139 +9,145 @@ export default class Times extends Component {
 
   constructor(props){
     super(props);
+
+
+    this.resize = () => {
+      drawChart({
+        target: 'mon',
+        type: 'Timeline',
+        columns: [
+          ['string', 'Employee'],
+          ['string', 'Job'],
+          ['date', 'Start'],
+          ['date', 'End']
+        ],
+        rows: [
+          ['Il Buono', "Mohave", new Date(2017, 10, 23, 8, 0), new Date(2017, 10, 23, 12, 30)],
+          ['Il Buono', "Mohave", new Date(2017, 10, 23, 13, 30), new Date(2017, 10, 23, 14, 30)],
+          ['Il Buono', "Clear Waters", new Date(2017, 10, 23, 14, 30), new Date(2017, 10, 23, 17, 0)],
+          ['Il Brutto', "Mohave", new Date(2017, 10, 23, 8, 0), new Date(2017, 10, 23, 12, 30)],
+          ['Il Brutto', "Clear Waters", new Date(2017, 10, 23, 13, 30), new Date(2017, 10, 23, 17, 0)],
+          ['Il Cattivo', "Federal Bank", new Date(2017, 10, 23, 8, 30), new Date(2017, 10, 23, 14, 0)]
+        ]
+      })
+      drawChart({
+        target: 'tue',
+        type: 'Timeline',
+        columns: [
+          ['string', 'Employee'],
+          ['string', 'Job'],
+          ['date', 'Start'],
+          ['date', 'End']
+        ],
+        rows: [
+          ['Il Buono', "Mohave", new Date(2017, 10, 23, 8, 0), new Date(2017, 10, 23, 12, 30)],
+          ['Il Buono', "Mohave", new Date(2017, 10, 23, 13, 30), new Date(2017, 10, 23, 14, 30)],
+          ['Il Buono', "Clear Waters", new Date(2017, 10, 23, 14, 30), new Date(2017, 10, 23, 17, 0)],
+          ['Il Brutto', "Mohave", new Date(2017, 10, 23, 8, 0), new Date(2017, 10, 23, 12, 30)],
+          ['Il Brutto', "Clear Waters", new Date(2017, 10, 23, 13, 30), new Date(2017, 10, 23, 17, 0)],
+          ['Il Cattivo', "Federal Bank", new Date(2017, 10, 23, 8, 30), new Date(2017, 10, 23, 14, 0)]
+        ]
+      })
+      drawChart({
+        target: 'wed',
+        type: 'Timeline',
+        columns: [
+          ['string', 'Employee'],
+          ['string', 'Job'],
+          ['date', 'Start'],
+          ['date', 'End']
+        ],
+        rows: [
+          ['Il Buono', "Mohave", new Date(2017, 10, 23, 8, 0), new Date(2017, 10, 23, 12, 30)],
+          ['Il Buono', "Mohave", new Date(2017, 10, 23, 13, 30), new Date(2017, 10, 23, 14, 30)],
+          ['Il Buono', "Clear Waters", new Date(2017, 10, 23, 14, 30), new Date(2017, 10, 23, 17, 0)],
+          ['Il Brutto', "Mohave", new Date(2017, 10, 23, 8, 0), new Date(2017, 10, 23, 12, 30)],
+          ['Il Brutto', "Clear Waters", new Date(2017, 10, 23, 13, 30), new Date(2017, 10, 23, 17, 0)],
+          ['Il Cattivo', "Federal Bank", new Date(2017, 10, 23, 8, 30), new Date(2017, 10, 23, 14, 0)]
+        ]
+      })
+      drawChart({
+        target: 'thu',
+        type: 'Timeline',
+        columns: [
+          ['string', 'Employee'],
+          ['string', 'Job'],
+          ['date', 'Start'],
+          ['date', 'End']
+        ],
+        rows: [
+          ['Il Buono', "Mohave", new Date(2017, 10, 23, 8, 0), new Date(2017, 10, 23, 12, 30)],
+          ['Il Buono', "Mohave", new Date(2017, 10, 23, 13, 30), new Date(2017, 10, 23, 14, 30)],
+          ['Il Buono', "Clear Waters", new Date(2017, 10, 23, 14, 30), new Date(2017, 10, 23, 17, 0)],
+          ['Il Brutto', "Mohave", new Date(2017, 10, 23, 8, 0), new Date(2017, 10, 23, 12, 30)],
+          ['Il Brutto', "Clear Waters", new Date(2017, 10, 23, 13, 30), new Date(2017, 10, 23, 17, 0)],
+          ['Il Cattivo', "Federal Bank", new Date(2017, 10, 23, 8, 30), new Date(2017, 10, 23, 14, 0)]
+        ]
+      })
+      drawChart({
+        target: 'fri',
+        type: 'Timeline',
+        columns: [
+          ['string', 'Employee'],
+          ['string', 'Job'],
+          ['date', 'Start'],
+          ['date', 'End']
+        ],
+        rows: [
+          ['Il Buono', "Mohave", new Date(2017, 10, 23, 8, 0), new Date(2017, 10, 23, 12, 30)],
+          ['Il Buono', "Mohave", new Date(2017, 10, 23, 13, 30), new Date(2017, 10, 23, 14, 30)],
+          ['Il Buono', "Clear Waters", new Date(2017, 10, 23, 14, 30), new Date(2017, 10, 23, 17, 0)],
+          ['Il Brutto', "Mohave", new Date(2017, 10, 23, 8, 0), new Date(2017, 10, 23, 12, 30)],
+          ['Il Brutto', "Clear Waters", new Date(2017, 10, 23, 13, 30), new Date(2017, 10, 23, 17, 0)],
+          ['Il Cattivo', "Federal Bank", new Date(2017, 10, 23, 8, 30), new Date(2017, 10, 23, 14, 0)]
+        ]
+      })
+      drawChart({
+        target: 'sat',
+        type: 'Timeline',
+        columns: [
+          ['string', 'Employee'],
+          ['string', 'Job'],
+          ['date', 'Start'],
+          ['date', 'End']
+        ],
+        rows: [
+          ['Il Buono', "Mohave", new Date(2017, 10, 23, 8, 0), new Date(2017, 10, 23, 12, 30)],
+          ['Il Buono', "Mohave", new Date(2017, 10, 23, 13, 30), new Date(2017, 10, 23, 14, 30)],
+          ['Il Buono', "Clear Waters", new Date(2017, 10, 23, 14, 30), new Date(2017, 10, 23, 17, 0)],
+          ['Il Brutto', "Mohave", new Date(2017, 10, 23, 8, 0), new Date(2017, 10, 23, 12, 30)],
+          ['Il Brutto', "Clear Waters", new Date(2017, 10, 23, 13, 30), new Date(2017, 10, 23, 17, 0)],
+          ['Il Cattivo', "Federal Bank", new Date(2017, 10, 23, 8, 30), new Date(2017, 10, 23, 14, 0)]
+        ]
+      })
+      drawChart({
+        target: 'sun',
+        type: 'Timeline',
+        columns: [
+          ['string', 'Employee'],
+          ['string', 'Job'],
+          ['date', 'Start'],
+          ['date', 'End']
+        ],
+        rows: [
+          ['Il Buono', "Mohave", new Date(2017, 10, 23, 8, 0), new Date(2017, 10, 23, 12, 30)],
+          ['Il Buono', "Mohave", new Date(2017, 10, 23, 13, 30), new Date(2017, 10, 23, 14, 30)],
+          ['Il Buono', "Clear Waters", new Date(2017, 10, 23, 14, 30), new Date(2017, 10, 23, 17, 0)],
+          ['Il Brutto', "Mohave", new Date(2017, 10, 23, 8, 0), new Date(2017, 10, 23, 12, 30)],
+          ['Il Brutto', "Clear Waters", new Date(2017, 10, 23, 13, 30), new Date(2017, 10, 23, 17, 0)],
+          ['Il Cattivo', "Federal Bank", new Date(2017, 10, 23, 8, 30), new Date(2017, 10, 23, 14, 0)]
+        ]
+      })
+    }
   }
 
+
   componentDidMount(){
-    drawChart({
-      target: 'mon',
-      type: 'Timeline',
-      columns: [
-        ['string', 'Employee'],
-        ['string', 'Job'],
-        ['date', 'Start'],
-        ['date', 'End']
-      ],
-      rows: [
-        ['Il Buono', "Mohave", new Date(2017, 10, 23, 8, 0), new Date(2017, 10, 23, 12, 30)],
-        ['Il Buono', "Mohave", new Date(2017, 10, 23, 13, 30), new Date(2017, 10, 23, 14, 30)],
-        ['Il Buono', "Clear Waters", new Date(2017, 10, 23, 14, 30), new Date(2017, 10, 23, 17, 0)],
-        ['Il Brutto', "Mohave", new Date(2017, 10, 23, 8, 0), new Date(2017, 10, 23, 12, 30)],
-        ['Il Brutto', "Clear Waters", new Date(2017, 10, 23, 13, 30), new Date(2017, 10, 23, 17, 0)],
-        ['Il Cattivo', "Federal Bank", new Date(2017, 10, 23, 8, 30), new Date(2017, 10, 23, 14, 0)]
-      ]
-    })
-    drawChart({
-      target: 'tue',
-      type: 'Timeline',
-      columns: [
-        ['string', 'Employee'],
-        ['string', 'Job'],
-        ['date', 'Start'],
-        ['date', 'End']
-      ],
-      rows: [
-        ['Il Buono', "Mohave", new Date(2017, 10, 23, 8, 0), new Date(2017, 10, 23, 12, 30)],
-        ['Il Buono', "Mohave", new Date(2017, 10, 23, 13, 30), new Date(2017, 10, 23, 14, 30)],
-        ['Il Buono', "Clear Waters", new Date(2017, 10, 23, 14, 30), new Date(2017, 10, 23, 17, 0)],
-        ['Il Brutto', "Mohave", new Date(2017, 10, 23, 8, 0), new Date(2017, 10, 23, 12, 30)],
-        ['Il Brutto', "Clear Waters", new Date(2017, 10, 23, 13, 30), new Date(2017, 10, 23, 17, 0)],
-        ['Il Cattivo', "Federal Bank", new Date(2017, 10, 23, 8, 30), new Date(2017, 10, 23, 14, 0)]
-      ]
-    })
-    drawChart({
-      target: 'wed',
-      type: 'Timeline',
-      columns: [
-        ['string', 'Employee'],
-        ['string', 'Job'],
-        ['date', 'Start'],
-        ['date', 'End']
-      ],
-      rows: [
-        ['Il Buono', "Mohave", new Date(2017, 10, 23, 8, 0), new Date(2017, 10, 23, 12, 30)],
-        ['Il Buono', "Mohave", new Date(2017, 10, 23, 13, 30), new Date(2017, 10, 23, 14, 30)],
-        ['Il Buono', "Clear Waters", new Date(2017, 10, 23, 14, 30), new Date(2017, 10, 23, 17, 0)],
-        ['Il Brutto', "Mohave", new Date(2017, 10, 23, 8, 0), new Date(2017, 10, 23, 12, 30)],
-        ['Il Brutto', "Clear Waters", new Date(2017, 10, 23, 13, 30), new Date(2017, 10, 23, 17, 0)],
-        ['Il Cattivo', "Federal Bank", new Date(2017, 10, 23, 8, 30), new Date(2017, 10, 23, 14, 0)]
-      ]
-    })
-    drawChart({
-      target: 'thu',
-      type: 'Timeline',
-      columns: [
-        ['string', 'Employee'],
-        ['string', 'Job'],
-        ['date', 'Start'],
-        ['date', 'End']
-      ],
-      rows: [
-        ['Il Buono', "Mohave", new Date(2017, 10, 23, 8, 0), new Date(2017, 10, 23, 12, 30)],
-        ['Il Buono', "Mohave", new Date(2017, 10, 23, 13, 30), new Date(2017, 10, 23, 14, 30)],
-        ['Il Buono', "Clear Waters", new Date(2017, 10, 23, 14, 30), new Date(2017, 10, 23, 17, 0)],
-        ['Il Brutto', "Mohave", new Date(2017, 10, 23, 8, 0), new Date(2017, 10, 23, 12, 30)],
-        ['Il Brutto', "Clear Waters", new Date(2017, 10, 23, 13, 30), new Date(2017, 10, 23, 17, 0)],
-        ['Il Cattivo', "Federal Bank", new Date(2017, 10, 23, 8, 30), new Date(2017, 10, 23, 14, 0)]
-      ]
-    })
-    drawChart({
-      target: 'fri',
-      type: 'Timeline',
-      columns: [
-        ['string', 'Employee'],
-        ['string', 'Job'],
-        ['date', 'Start'],
-        ['date', 'End']
-      ],
-      rows: [
-        ['Il Buono', "Mohave", new Date(2017, 10, 23, 8, 0), new Date(2017, 10, 23, 12, 30)],
-        ['Il Buono', "Mohave", new Date(2017, 10, 23, 13, 30), new Date(2017, 10, 23, 14, 30)],
-        ['Il Buono', "Clear Waters", new Date(2017, 10, 23, 14, 30), new Date(2017, 10, 23, 17, 0)],
-        ['Il Brutto', "Mohave", new Date(2017, 10, 23, 8, 0), new Date(2017, 10, 23, 12, 30)],
-        ['Il Brutto', "Clear Waters", new Date(2017, 10, 23, 13, 30), new Date(2017, 10, 23, 17, 0)],
-        ['Il Cattivo', "Federal Bank", new Date(2017, 10, 23, 8, 30), new Date(2017, 10, 23, 14, 0)]
-      ]
-    })
-    drawChart({
-      target: 'sat',
-      type: 'Timeline',
-      columns: [
-        ['string', 'Employee'],
-        ['string', 'Job'],
-        ['date', 'Start'],
-        ['date', 'End']
-      ],
-      rows: [
-        ['Il Buono', "Mohave", new Date(2017, 10, 23, 8, 0), new Date(2017, 10, 23, 12, 30)],
-        ['Il Buono', "Mohave", new Date(2017, 10, 23, 13, 30), new Date(2017, 10, 23, 14, 30)],
-        ['Il Buono', "Clear Waters", new Date(2017, 10, 23, 14, 30), new Date(2017, 10, 23, 17, 0)],
-        ['Il Brutto', "Mohave", new Date(2017, 10, 23, 8, 0), new Date(2017, 10, 23, 12, 30)],
-        ['Il Brutto', "Clear Waters", new Date(2017, 10, 23, 13, 30), new Date(2017, 10, 23, 17, 0)],
-        ['Il Cattivo', "Federal Bank", new Date(2017, 10, 23, 8, 30), new Date(2017, 10, 23, 14, 0)]
-      ]
-    })
-    drawChart({
-      target: 'sun',
-      type: 'Timeline',
-      columns: [
-        ['string', 'Employee'],
-        ['string', 'Job'],
-        ['date', 'Start'],
-        ['date', 'End']
-      ],
-      rows: [
-        ['Il Buono', "Mohave", new Date(2017, 10, 23, 8, 0), new Date(2017, 10, 23, 12, 30)],
-        ['Il Buono', "Mohave", new Date(2017, 10, 23, 13, 30), new Date(2017, 10, 23, 14, 30)],
-        ['Il Buono', "Clear Waters", new Date(2017, 10, 23, 14, 30), new Date(2017, 10, 23, 17, 0)],
-        ['Il Brutto', "Mohave", new Date(2017, 10, 23, 8, 0), new Date(2017, 10, 23, 12, 30)],
-        ['Il Brutto', "Clear Waters", new Date(2017, 10, 23, 13, 30), new Date(2017, 10, 23, 17, 0)],
-        ['Il Cattivo', "Federal Bank", new Date(2017, 10, 23, 8, 30), new Date(2017, 10, 23, 14, 0)]
-      ]
-    })
+    this.resize()
   }
 
   render() {
-
+    window.onresize = this.resize;
     return (
       <div className="md-grid--40-16">
         <Card tableCard>
@@ -151,43 +157,43 @@ export default class Times extends Component {
               <TableRow>
                 <TableColumn>MON</TableColumn>
                 <TableColumn grow>
-                  <div id="mon"></div>
+                  <div id="mon" className="timeline"></div>
                 </TableColumn>
               </TableRow>
               <TableRow>
                 <TableColumn>TUE</TableColumn>
                 <TableColumn grow>
-                  <div id="tue"></div>
+                  <div id="tue" className="timeline"></div>
                 </TableColumn>
               </TableRow>
               <TableRow>
                 <TableColumn>WED</TableColumn>
                 <TableColumn grow>
-                  <div id="wed"></div>
+                  <div id="wed" className="timeline"></div>
                 </TableColumn>
               </TableRow>
               <TableRow>
                 <TableColumn>THU</TableColumn>
                 <TableColumn grow>
-                  <div id="thu"></div>
+                  <div id="thu" className="timeline"></div>
                 </TableColumn>
               </TableRow>
               <TableRow>
                 <TableColumn>FRI</TableColumn>
                 <TableColumn grow>
-                  <div id="fri"></div>
+                  <div id="fri" className="timeline"></div>
                 </TableColumn>
               </TableRow>
               <TableRow>
                 <TableColumn>SAT</TableColumn>
                 <TableColumn grow>
-                  <div id="sat"></div>
+                  <div id="sat" className="timeline"></div>
                 </TableColumn>
               </TableRow>
               <TableRow>
                 <TableColumn>SUN</TableColumn>
                 <TableColumn grow>
-                  <div id="sun"></div>
+                  <div id="sun" className="timeline"></div>
                 </TableColumn>
               </TableRow>
             </TableBody>
